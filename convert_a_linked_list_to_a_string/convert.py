ceae71ab86221ea2e31a7c817d390ab25db0cb7f
Oleksii-Lasiichuk <oleks1297@gmail.com>
2025-04-01 17:18:00 +0300

    added nedded code

--- convert_a_linked_list_to_a_string/convert.py
+++ convert_a_linked_list_to_a_string/convert.py
@@ -1,3 +1,8 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
 def stringify(node):
     if node is None:
         return "None"
--- parse_a_linked_list_from_a_string/parsing.py
+++ parse_a_linked_list_from_a_string/parsing.py
@@ -1,14 +1,15 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
 def linked_list_from_string(s):
     all_nodes_data = s.split(' -> ')
-    lst = [Node(all_nodes_data[0])]
-    for i, note in enumerate(all_nodes_data):
-        if i == 0:
-            continue
-        if i == len(all_nodes_data) - 1:
-            return lst[0]
-        new_node = Node(note)
-        lst[-1].next = new_node
-        lst.append(new_node)
-        
-        
-    return lst[0]
+    if len(all_nodes_data) == 1:
+        return None
+    head = Node(int(all_nodes_data[0]))
+    current = head
+    for data in all_nodes_data[1:-1]:  # Skip the first (already processed) and last ("None")
+        current.next = Node(int(data))
+        current = current.next
+    return head
