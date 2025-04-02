8777b9a015fa083f7e271d566a93c7120b8e85be
Oleksii-Lasiichuk <oleks1297@gmail.com>
2025-04-02 21:03:55 +0300

    recursive reverse problem implementation

---
+++ linked_lists_recursive_reverse/reverse.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    prev = None
+    current = head
+    while current is not None:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    return prev
+
